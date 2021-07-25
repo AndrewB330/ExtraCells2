2fe23799fcd8757c39cdfbe6e5d963c3428534bc
Andrii Borziak <Andrey.Borzyak@gmail.com>
2021-07-24 22:37:01 -0700

    Fixed certus tank rendering in inventory

--- src/main/scala/extracells/render/item/ItemRendererCertusTank.java
+++ src/main/scala/extracells/render/item/ItemRendererCertusTank.java
@@ -32,6 +32,7 @@ public class ItemRendererCertusTank implements IItemRenderer {
 		GL11.glEnable(GL11.GL_CULL_FACE);
 		GL11.glEnable(GL11.GL_LIGHTING);
 		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glEnable(GL11.GL_ALPHA_TEST);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
 		GL11.glTranslatef(0.5F, 0.5F, 0.5F);
